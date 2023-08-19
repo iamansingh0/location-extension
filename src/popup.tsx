ff10acb786049d8cd73c5138e96b3a1ff68e4630
iamansingh0 <amankumar7355197337@gmail.com>
2023-08-19 22:54:28 +0530

    update

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import axios from 'axios'
 import "~style.css"
 import dotenv from "dotenv";
-dotenv.config({ path: "src/config/config.env" });
+dotenv.config();
 
 interface IpData {
   ip: string;
@@ -27,7 +27,8 @@ function IndexPopup() {
   const IP_URL = 'https://api.ipify.org?format=json'
   useEffect(() => {
     if (ipData) {
-      const Detail_URL = `https://ipinfo.io/${ipData.ip}?token=110b93d9eb46c2`;
+      const token : string | undefined  = process.env.PLASMO_PUBLIC_LOCATION_TOKEN
+      const Detail_URL = `https://ipinfo.io/${ipData.ip}?token=${token}`;
       axios.get<Location>(Detail_URL).then((res2) => {
         res2.data.country = GetNation(res2.data.country);
         setLocationData(res2.data);
@@ -57,21 +58,21 @@ function IndexPopup() {
   };
 
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-[500px] plasmo-w-[500px] plasmo-bg-[#2293B0] plasmo-flex-col">
-      <div className="plasmo-absolute plasmo-top-28">
+    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-[500px] plasmo-w-[500px] plasmo-bg-gradient-to-br plasmo-from-[#09090F] plasmo-via-[#100F3B] plasmo-to-[#1A176E] plasmo-flex-col">
+      <div className="plasmo-absolute plasmo-top-20">
         {locationData && (
-          <div>
+          <div className="plasmo-rounded-lg plasmo-border-2 plasmo-border-white plasmo-border-solid  plasmo-p-[12px] plasmo-w-[250px] plasmo-flex plasmo-flex-col plasmo-justify-center plasmo-items-center">
             <h2 className="plasmo-text-base plasmo-text-slate-50">Your country is {locationData.country}</h2>
             <h2 className="plasmo-text-base plasmo-text-slate-50">and city is {locationData.city}</h2>
-            {/* <h2>{getToken}</h2> */}
           </div>
         )}
       </div>
       <button
         type="button"
         className="plasmo-flex plasmo-flex-row plasmo-items-center plasmo-justify-center plasmo-px-4 plasmo-py-2 plasmo-text-base plasmo-transition-all plasmo-border-none
-      plasmo-shadow-lg hover:plasmo-shadow-md plasmo-font-bold
-      active:plasmo-scale-105 plasmo-bg-slate-50 hover:plasmo-bg-slate-100 plasmo-text-slate-800 hover:plasmo-text-slate-900 plasmo-w-[200px] plasmo-h-[50px]"
+      plasmo-shadow-lg plasmo-shadow-md plasmo-font-small plasmo-rounded plasmo-font-semibold 
+      hover:plasmo-font-bold
+      active:plasmo-scale-105 plasmo-bg-slate-50 hover:plasmo-bg-slate-100 plasmo-text-slate-800 hover:plasmo-text-slate-900 plasmo-w-[180px] plasmo-h-[40px] hover:plasmo-w-[200px] hover:plasmo-h-[50px]"
       onClick={buttonHandler} name="btn1" disabled={isLoading}>
         {isLoading ? "Loading..." : buttonText}
       </button>
